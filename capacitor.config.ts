71ea43495c15ff9654fe327d5ed1de695c1c6d83
Starlyn camarena <scamarena0823@gmail.com>
2026-09-13 01:02:21 -0400

    Use dedicated web assets folder for Android build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.mediousao.app',
   appName: 'MEDIOUSAO',
-  webDir: '.',
+  webDir: 'www',
   bundledWebRuntime: false,
   server: {
     androidScheme: 'https'
